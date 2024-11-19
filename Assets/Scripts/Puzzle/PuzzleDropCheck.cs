7c7f864b7930d1829e94db4109a7e846245596f1
jalerse <jalerse@gmail.com>
2024-11-19 19:52:50 +0700

    PuzzleDropCheck: return the puzzle
    * if the puzzle isn't in a valid placement, return it into initial position

---
+++ Assets/Scripts/Puzzle/PuzzleDropCheck.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Assets.Scripts.Puzzle
+{
+    public class PuzzleDropCheck : MonoBehaviour, IEndDragHandler
+    {
+        private Vector2 _initialPosition;
+        public bool _IsValidDrop { get; set; }
+        private bool _isValidDrop => _IsValidDrop;
+
+        private void Awake()
+        {
+            _initialPosition = transform.localPosition;
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            if (!_isValidDrop)
+            {
+                transform.localPosition = _initialPosition;
+            }
+        }
+
+    }
+}
