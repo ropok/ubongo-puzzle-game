0eaccfb9273467795dfa629744e2e7cea6b9038e
jalerse <jalerse@gmail.com>
2024-11-20 04:58:42 +0700

    Puzzle release check
    * check if the puzzle already exists in the base

--- Assets/Scripts/Puzzle/PuzzleBase/PuzzleBase.cs
+++ Assets/Scripts/Puzzle/PuzzleBase/PuzzleBase.cs
@@ -6,14 +6,35 @@ namespace Assets.Scripts.Puzzle.PuzzleBase
     public class PuzzleBase : MonoBehaviour
     {
         private bool _isOccupied;
+        private GameObject _droppedPuzzle;
 
-        private void OnTriggerEnter2D(Collider2D collider)
+        private void OnTriggerEnter2D(Collider2D collision)
         {
             if (_isOccupied) return;
 
-            Debug.Log($"TriggerEnter: {collider.gameObject.name}");
-            IDroppable droppable = collider.GetComponent<IDroppable>();
+            Debug.Log($"TriggerEnter: {collision.gameObject.name}");
+            IDroppable droppable = collision.GetComponent<IDroppable>();
             droppable.DroppingPuzzle();
+
+            _isOccupied = true;
+
+            _droppedPuzzle = collision.gameObject;
+
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            // if it's not occupied yet, no need to do exit detection
+            if (!_isOccupied) return;
+
+
+            IDroppable droppable = collision.GetComponent<IDroppable>();
+            bool isSamePuzzle = _droppedPuzzle == collision.gameObject;
+            if (droppable != null && isSamePuzzle)
+            {
+                _isOccupied = false;
+                _droppedPuzzle = null;
+            }
 
         }
     }
